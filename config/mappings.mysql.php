5e8a0d166aa934588f1e11f2c657d61129027f7d
pintsizeDev <danDev@pintsize.it>
2015-08-27 11:48:25 +0100

    Lets hope the one implemented is called that

--- config/mappings.mysql.php
+++ config/mappings.mysql.php
@@ -1,6 +1,7 @@
 <?php
 use Clearbooks\Labs\Release\Gateway\ReleaseGateway;
 use Clearbooks\Labs\Release\Gateway\ReleaseToggleCollection;
+use Clearbooks\Labs\Toggle\Gateway\ActivatedToggleGateway;
 use Clearbooks\Labs\Toggle\Gateway\UserToggleGateway;
 use Clearbooks\Labs\Toggle\Gateway\ActivatableToggleGateway;
 use Clearbooks\LabsMysql\Release\MysqlReleaseGateway;
@@ -9,6 +10,7 @@ use Clearbooks\LabsMysql\Toggle\MysqlUserToggleGateway;
 use Clearbooks\LabsMysql\Toggle\MysqlActivatableToggleGateway;
 use Clearbooks\Labs\Toggle\Gateway\GroupToggleGateway;
 use Clearbooks\LabsMysql\Toggle\MysqlGroupToggleGateway;
+use Clearbooks\LabsMysql\Toggle\MySqlActivatedToggleGateway;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 
@@ -18,7 +20,7 @@ return [
     UserToggleGateway::class => \Di\object(MysqlUserToggleGateway::class),
     ActivatableToggleGateway::class => \DI\object (MysqlActivatableToggleGateway::class),
     GroupToggleGateway::class => \Di\object(MysqlGroupToggleGateway::class),
-    //ActivatedToggleGateway::class => \Di\object(MysqlActivatedToggleGateway::class),
+    ActivatedToggleGateway::class => \Di\object(MysqlActivatedToggleGateway::class),
 
     Connection::class => function() {
         return DriverManager::getConnection([
