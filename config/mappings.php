28a570b21317cb9a7c18d6334ae8860368f8cb69
danielburnley <daniel@clearbooks.co.uk>
2015-09-02 12:28:03 +0100

    add a mapping

--- config/mappings.php
+++ config/mappings.php
@@ -1,8 +1,10 @@
 <?php
 use Emarref\Jwt\Algorithm\AlgorithmInterface;
 use Emarref\Jwt\Algorithm\Hs512;
+use Emarref\Jwt\Jwt;
 
 return [
     DateTimeInterface::class => new DateTime,
-    AlgorithmInterface::class => new Hs512("{{ encryption_secret_key }}")
+    AlgorithmInterface::class => new Hs512("{{ encryption_secret_key }}"),
+    Jwt::class => new JWt()
 ] + include "mappings.mysql.php";
